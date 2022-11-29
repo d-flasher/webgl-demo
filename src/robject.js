cd34ef506a0c7075ac277968e37c0b97fea61ed2
Sitdikov DR <sdr-u@mail.ru>
2022-11-29 15:32:51 +0500

    tt

--- src/robject.js
+++ src/robject.js
@@ -1,3 +1,12 @@
 export class RObject {
-  
+  static EVENT_CHANGED = 'changed'
+
+  create(target) {
+    return new Proxy(target, {
+      set(target, prop, val) {
+        console.log('pr', prop, val)
+        return Reflect.set(...arguments)
+      }
+    })
+  }
 }
--- src/robject.test.js
+++ src/robject.test.js
@@ -1,5 +1,14 @@
+import { RObject } from './robject.js'
+
 describe('RObject', () => {
-  test('read', ()=>{
+  test('read', () => {
     expect(true).toBeTruthy()
+
+    let target = {
+      v1: 1,
+      v2: 'qwerty',
+    }
+    target = new RObject().create(target)
+    target.v1 = 2
   })
 })
